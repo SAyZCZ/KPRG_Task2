ee719322bb031c84ea8e39a22178d1d0b0802dee
Adam <61239599+SAyZCZ@users.noreply.github.com>
2023-12-26 11:13:30 +0100

    Update LineRasterizeTrivial.java

--- task2_muj_13._12_23/task2/src/rasterize/LineRasterizeTrivial.java
+++ task2_muj_13._12_23/task2/src/rasterize/LineRasterizeTrivial.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class LineRasterizeTrivial extends LineRasterizer{
 
-    // konstruktor
+    
     public LineRasterizeTrivial(Raster raster){
         super(raster);
     }
@@ -12,7 +12,7 @@ public class LineRasterizeTrivial extends LineRasterizer{
     @Override
     public void rasterize(int x1, int y1, int x2, int y2, Color color,int gap) {
 
-        // POKUS 2 ***************************************************
+     
         float k = (float) (y2 - y1) / (x2 - x1);
         float q = y1 - k * x1;
         int temp;
@@ -43,60 +43,7 @@ public class LineRasterizeTrivial extends LineRasterizer{
                 raster.setPixel((int) x, y, color.getRGB());
             }
         }
-        // POKUS 1 ***************************************************
-        /* Dodelat popis trivialniho algoritmu*/
-        /*
-        int forStart = x1;
-        int forEnd = x2;
 
-        if (x2 - x1 != 0) {
-            float k = (y2 - y1) / (float) (x2 - x1);
-            float q = y1 - k * x1;
-            if (Math.abs(k) <= 1) {
-                if (x1 > x2) {
-                    forStart = x2;
-                    forEnd = x1;
-                }
-                for (int i = forStart; i < forEnd; i++) {
-                    float y = i * k + q;
-                    if ((i > 0 || i < raster.getWidth() - 1) && (Math.round(y) > 0 || Math.round(y) < raster.getHeight() - 1)) {
-                        raster.setPixel(i, Math.round(y), color.getRGB());
-                    }
-                }
-            } else {
-                if (y1 > y2) {
-                    forStart = y2;
-                    forEnd = y1;
-                } else {
-                    forStart = y1;
-                    forEnd = y2;
-                }
-                for (int i = forStart; i < forEnd; i++) {
-                    float x = (i - q) / k;
-                    if ((Math.round(x) > 0 || Math.round(x) < raster.getWidth() - 1) && (i > 0 || i < raster.getHeight() - 1)) {
-                        raster.setPixel(Math.round(x), i, color.getRGB());
-                    }
-                }
-
-            }
-
-
-        } else {
-            if (y2 > y1) {
-                forStart = y1;
-                forEnd = y2;
-            } else {
-                forStart = y2;
-                forEnd = y1;
-            }
-            for (int i = forStart; i <= forEnd; i++) {
-                if ((x1 > 0 || x1 < raster.getWidth() - 1) && (i > 0 || i < raster.getHeight() - 1)) {
-                    raster.setPixel(x1, i, color.getRGB());
-                }
-
-            }
-        }
-        */
     }
 }
 
